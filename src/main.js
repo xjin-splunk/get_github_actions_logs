f714c1a9cd85df7c7fcd13dc3730793379ebf1f9
xjin-splunk <xjin@splunk.com>
2022-07-22 17:33:06 -0700

    try

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("hello");
+import core from "@actions/core";
+import github from "@actions/github";
+import Octokit from '@octokit/rest';
+
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+const octokit = new Octokit({
+    auth: `${github.token}`
+})
+
+await octokit.request('GET /xjin-splunk/get_github_actions_logs/actions/runs', {
+    owner: 'xjin-splunk',
+    repo: 'get_github_actions_logs'
+})
