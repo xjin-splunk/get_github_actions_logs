1d4fa0754cb835c1e59cba1465e277dcea9c7fbc
xjin-splunk <xjin@splunk.com>
2022-07-25 13:44:00 -0700

    add sync

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import core from "@actions/core";
 import github from "@actions/github";
+import { composePaginateRest } from "@octokit/plugin-paginate-rest";
 import Octokit from '@octokit/rest';
 
-const githubToken = core.getInput("token");
+async function run(){
+    const githubToken = core.getInput("token");
 
-// Octokit.js
-// https://github.com/octokit/core.js#readme
-const octokit = github.getOctokit(githubToken);
+    // Octokit.js
+    // https://github.com/octokit/core.js#readme
+    const octokit = github.getOctokit(githubToken);
+    
+    const data = await octokit.request('GET /xjin-splunk/get_github_actions_logs/actions/runs', {
+        owner: 'xjin-splunk',
+        repo: 'get_github_actions_logs'
+    })
 
-await octokit.request('GET /xjin-splunk/get_github_actions_logs/actions/runs', {
-    owner: 'xjin-splunk',
-    repo: 'get_github_actions_logs'
-})
+    console.log(data);
+};
+
+run();
