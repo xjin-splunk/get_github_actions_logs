999b4350635e525c490a4d29a2eb53a901ff86b5
xjin-splunk <xjin@splunk.com>
2022-07-25 15:33:44 -0700

    add require

--- dist/index.js
+++ dist/index.js
@@ -9014,7 +9014,7 @@ async function run(){
     const octokit = github.getOctokit(githubToken);
     // const octokit = github.getOctokit('${github.token}');
     
-    await octokit.request('GET /xjin-splunk/get_github_actions_logs/actions/runs', {
+    await octokit.request('GET repos/xjin-splunk/get_github_actions_logs/actions/runs', {
         owner: 'xjin-splunk',
         repo: 'get_github_actions_logs'
     }).then((data) => {console.log(data)}).catch(console.log("get log failed"));
