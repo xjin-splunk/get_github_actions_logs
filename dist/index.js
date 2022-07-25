7b80aa66147a7cc9e992c1943dcb2ad4a2bcc3e4
xjin-splunk <xjin@splunk.com>
2022-07-25 13:52:41 -0700

    add catch

--- dist/index.js
+++ dist/index.js
@@ -9063,7 +9063,7 @@ async function run(){
     await octokit.request('GET /xjin-splunk/get_github_actions_logs/actions/runs', {
         owner: 'xjin-splunk',
         repo: 'get_github_actions_logs'
-    }).then((data) => {console.log(data)});
+    }).then((data) => {console.log(data)}).catch(concole.log("get log failed"));
 
     
 };
