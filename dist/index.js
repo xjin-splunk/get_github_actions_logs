9bd2e485f497ed2db3e676f1b2cf192e8fe644c5
xjin-splunk <xjin@splunk.com>
2022-07-25 15:31:34 -0700

    add require

--- dist/index.js
+++ dist/index.js
@@ -9007,17 +9007,17 @@ const github = __nccwpck_require__(5438);
 
 
 async function run(){
-    // const githubToken = core.getInput("token");
+    const githubToken = core.getInput("token");
 
     // Octokit.js
     // https://github.com/octokit/core.js#readme
-    // const octokit = github.getOctokit(githubToken);
-    const octokit = github.getOctokit('${github.token}');
+    const octokit = github.getOctokit(githubToken);
+    // const octokit = github.getOctokit('${github.token}');
     
     await octokit.request('GET /xjin-splunk/get_github_actions_logs/actions/runs', {
         owner: 'xjin-splunk',
         repo: 'get_github_actions_logs'
-    }).then((data) => {console.log(data)});
+    }).then((data) => {console.log(data)}).catch(console.log("get log failed"));
 
     
 };
--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ const github = require("@actions/github");
 
 
 async function run(){
-    // const githubToken = core.getInput("token");
+    const githubToken = core.getInput("token");
 
     // Octokit.js
     // https://github.com/octokit/core.js#readme
-    // const octokit = github.getOctokit(githubToken);
-    const octokit = github.getOctokit('${github.token}');
+    const octokit = github.getOctokit(githubToken);
+    // const octokit = github.getOctokit('${github.token}');
     
     await octokit.request('GET /xjin-splunk/get_github_actions_logs/actions/runs', {
         owner: 'xjin-splunk',
         repo: 'get_github_actions_logs'
-    }).then((data) => {console.log(data)});
+    }).then((data) => {console.log(data)}).catch(console.log("get log failed"));
 
     
 };
