6445ac3a033d4d470b7c61eddfaa6dad1433ec1f
xjin-splunk <xjin@splunk.com>
2022-07-26 13:43:48 -0700

    pass in parameters

--- src/main.js
+++ src/main.js
@@ -4,15 +4,17 @@ const github = require("@actions/github");
 
 async function run(){
     const githubToken = core.getInput("token");
+    const repo_name = core.getInput("repo_name");
+    const repo_owner = core.getInput("repo_owner");
 
     // Octokit.js
     // https://github.com/octokit/core.js#readme
     const octokit = github.getOctokit(githubToken);
     // const octokit = github.getOctokit('${github.token}');
     
-    await octokit.request('GET /repos/xjin-splunk/get_github_actions_logs/actions/runs', {
-        owner: 'xjin-splunk',
-        repo: 'get_github_actions_logs'
+    await octokit.request('GET /repos/'+repo_owner+'/'+repo_name+'/actions/runs', {
+        owner: `${repo_owner}`,
+        repo: `${repo_name}`
     }).then((data) => {console.log(data)}).catch(console.log("get log failed"));
 };
 
